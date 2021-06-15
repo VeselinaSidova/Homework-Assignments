bd3e1f1ad8b344e8bcef25a7326443e2be144a8a
Veselina Sidova <vessy.sidova@gmail.com>
2021-06-16 01:15:10 +0300

    Added Sets and Dictionaries Advanced - Exercise

---
+++ C# Homework Assignments/C# Advanced/Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = nm[0];
+            int m = nm[1];
+            HashSet<string> set1 = new HashSet<string>();
+            HashSet<string> set2 = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                set1.Add(Console.ReadLine());
+            }
+            for (int j = 0; j < m; j++)
+            {
+                set2.Add(Console.ReadLine());
+            }
+            Console.WriteLine(string.Join(" ", set1.Intersect(set2)));
+        }
+    }
+}
---
+++ C# Homework Assignments/C# Advanced/Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<string> elements = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                foreach (var item in input)
+                {
+                    elements.Add(item);
+                }
+            }
+            Console.WriteLine(string.Join(' ', elements));
+        }
+    }
+}
