33bcf7fe5f1b49a240dd6c92d5a64a5874066e9b
Veselina Sidova <vessy.sidova@gmail.com>
2023-07-13 02:08:01 +0300

    added exercises

---
+++ C# Homework Assignments/C# Fundamentals/IntroAndBasicSyntaxLab/11.MultiplicationTable2.0/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _11.MultiplicationTable2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                if (m <= i)
+                {
+                    Console.WriteLine($"{n} X {i} = {n * i}");
+                }
+
+            }
+
+            if (m > 10)
+            {
+                Console.WriteLine($"{n} X {m} = {n * m}");
+            }
+        }
+    }
+}
