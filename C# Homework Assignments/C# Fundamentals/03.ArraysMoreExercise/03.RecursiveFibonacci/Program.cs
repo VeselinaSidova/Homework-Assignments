1d7aa9f453bc34eaf19fe986d7a60fe005b980b0
Veselina Sidova <vessy.sidova@gmail.com>
2023-11-14 01:30:25 +0200

    fixed numbering

---
+++ C# Homework Assignments/C# Fundamentals/03.ArraysMoreExercise/03.RecursiveFibonacci/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _03.RecursiveFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 5.5;
+            //Console.WriteLine($"{number:D2}");
+            Console.WriteLine($"{number:F2}");
+        }
+    }
+}
--- JS Homework Assignments/JS Advanced/SyntaxFunctionsAndStatementsExercise/5.TimeToWalk.js
+++
@@ -1,5 +0,0 @@
-function solve(steps, meters, kmPerHour) {
-
-}
-
-solve(4000, 0.60, 5);
