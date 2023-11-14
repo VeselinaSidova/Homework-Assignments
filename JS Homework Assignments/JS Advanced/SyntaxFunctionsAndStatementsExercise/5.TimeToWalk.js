223352f2b0e0d004f8994913a6d9c40a21652786
Veselina Sidova <vessy.sidova@gmail.com>
2023-11-14 05:01:01 +0200

    added missing exercise

--- JS Homework Assignments/JS Advanced/SyntaxFunctionsAndStatementsExercise/5.TimeToWalk.js
+++ JS Homework Assignments/JS Advanced/SyntaxFunctionsAndStatementsExercise/5.TimeToWalk.js
@@ -1,5 +1,15 @@
-function solve(steps, meters, kmPerHour) {
+function solve(steps, footprint, speed) {
+    let meters = steps * footprint;
+    let timeInSeconds = Math.round((meters / 1000) / speed * 3600);
+    let stops = Math.floor(meters / 500);
+    let totalTimeInSeconds = (stops * 60) + timeInSeconds;
 
+    let hours = Math.floor(totalTimeInSeconds / 3600);
+    let minutes = Math.floor(totalTimeInSeconds / 60);
+    let seconds = totalTimeInSeconds % 60;
+
+    console.log(`${hours < 10 ? 0 : ""}${hours}:${minutes < 10 ? 0 : ""}${minutes}:${seconds < 10 ? 0 : ""}${seconds}`);
 }
 
 solve(4000, 0.60, 5);
+solve(2564, 0.70, 5.5);
